39d744aed4319f09fffeaf9125f12ba80831c104
Mario <mariogr901@gmail.com>
2022-12-08 20:37:35 +0100

    Pedidos funcionales.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -91,233 +91,235 @@ fun main(args: Array<String>) {
     tareasInit.forEach { tarea ->
         tareaController.createTarea(tarea)
 
-    val pedidosInit = getPedidosInit()
-    pedidosInit.forEach { pedidos ->
-        pedidoController.createPedido(pedidos)
+        val pedidosInit = getPedidosInit()
+        pedidosInit.forEach { pedidos ->
+            pedidoController.createPedido(pedidos)
+
+        }
+
+        //CRUD
+        //Producto
+        //FindAll
+        val producto = productoController.getProductos()
+        producto.forEach { println(it) }
+        //FindById
+        val productoId = productoController.getProductoById(producto[0].id)
+        productoId?.let { println(it) }
+        //Update
+        val productoUpdate = productoController.getProductoById(producto[0].id)
+        productoUpdate?.let {
+            it.stock += 3
+            productoController.updateProducto(it)
+        }
+        println(productoUpdate)
+        //Delete
+        val productoDelete = productoController.getProductoById(producto[0].id)
+        productoDelete?.let { if (productoController.deleteProducto(it)) println("Producto eliminado") }
+
+        //Adquisicion
+        //FindAll
+        val adquisicion = adquisicionController.getAdquisiciones()
+        adquisicion.forEach { println(it) }
+        //FindById
+        val adquisicionId = adquisicionController.getAdquisicionById(adquisicion[0].id)
+        adquisicionId?.let { println(it) }
+        //Update
+        val adquisicionUpdate = adquisicionController.getAdquisicionById(adquisicion[0].id)
+        adquisicionUpdate?.let {
+            it.cantidad += 1
+            it.precio = it.precio?.times(it.cantidad)
+            adquisicionController.updateAdquisicion(it)
+        }
+        println(adquisicionUpdate)
+        //Delete
+        val adquisicionDelete = adquisicionController.getAdquisicionById(adquisicion[0].id)
+        adquisicionDelete?.let { if (adquisicionController.deleteAdquisicion(it)) println("Adquisicion eliminada") }
+
+        //Encordar
+        //FindAll
+        val encordar = encordarController.getEncordados()
+        encordar.forEach { println(it) }
+        //FindById
+        val encordadoId = encordarController.getEncordadoById(encordar[0].id)
+        encordadoId?.let { println(it) }
+        //Update
+        val encordadoUpdate = encordarController.getEncordadoById(encordar[0].id)
+        encordadoUpdate?.let {
+            it.nudos += 2
+            encordarController.updateEncordado(it)
+        }
+        println(encordadoUpdate)
+        //Delete
+        val encordadoDelete = encordarController.getEncordadoById(encordar[0].id)
+        encordadoDelete?.let { if (encordarController.deleteEncordado(it)) println("Encordado eliminado") }
+
+        //Personalizar
+        //FindAll
+        val personalizar = personalizarController.getPersonalizaciones()
+        personalizar.forEach { println(it) }
+        //FindById
+        val personalizarId = personalizarController.getPersonalizacionById(personalizar[0].id)
+        personalizarId?.let { println(it) }
+        //Update
+        val personalizarUpdate = personalizarController.getPersonalizacionById(personalizar[0].id)
+        personalizarUpdate?.let {
+            it.peso += 1
+            personalizarController.updatePersonalizacion(personalizarUpdate)
+        }
+        println(personalizarUpdate)
+        //Delete
+        val personalizarDelete = personalizarController.getPersonalizacionById(personalizar[0].id)
+        personalizarDelete?.let { if (personalizarController.deletePersonalizacion(it)) println("Personalización eliminada") }
+
+        //Encargado
+        //FindAll
+        val encargado = encargadoController.getEncargados()
+        encargado.forEach { println(it) }
+        //FindById
+        val encargadoId = encargadoController.getEncargadoById(encargado[0].id)
+        encargadoId?.let { println(it) }
+        //Update
+        val encargadoUpdate = encargadoController.getEncargadoById(encargado[0].id)
+        encargadoUpdate?.let {
+            it.nombre = "Ernesto"
+            encargadoController.updateEncargado(it)
+        }
+        println(encargadoUpdate)
+        //Delete
+        val encargadoDelete = encargadoController.getEncargadoById(encargado[0].id)
+        encargadoDelete?.let { if (encargadoController.deleteEncargado(it)) println("Encargado eliminado") }
+
+        //Cliente
+        //FindAll
+        val cliente = clienteController.getClientes()
+        cliente.forEach { println(it) }
+        //FindById
+        val clienteId = clienteController.getClienteById(cliente[0].id)
+        clienteId?.let { println(it) }
+        //Update
+        val clienteUpdate = clienteController.getClienteById(cliente[0].id)
+        clienteUpdate?.let {
+            it.apellido = "Acosta"
+            clienteController.updateCliente(it)
+        }
+        println(clienteUpdate)
+        //Delete
+        val clienteDelete = clienteController.getClienteById(cliente[1].id)
+        clienteDelete?.let { if (clienteController.deleteCliente(it)) println("Cliente eliminado") }
+
+        //Pedido
+        //FindAll
+        val pedidos = pedidoController.getPedidos()
+        pedidos.forEach { println(it) }
+        //FindById
+        val pedidoId = pedidoController.getPedidoById(pedidos[0].id)
+        pedidoId?.let { println(it) }
+        //Update
+        val pedidoUpdate = pedidoController.getPedidoById(pedidos[0].id)
+        pedidoUpdate?.let {
+            it.estado = Pedido.TipoEstado.PROCESANDO
+            pedidoController.updatePedido(it)
+        }
+        println(pedidoUpdate)
+        //Delete
+        val pedidoDelete = pedidoController.getPedidoById(pedidos[1].id)
+        pedidoDelete?.let { if (pedidoController.deletePedido(it)) println("Pedido eliminado") }
+
+        //Trabajador
+        //FindAll
+        val trabajador = trabajadorController.getTrabajadores()
+        trabajador.forEach { println(it) }
+        //FindById
+        val trabajadorId = trabajadorController.getTrabajadorById(trabajador[0].id)
+        trabajadorId?.let { println(it) }
+        //Update
+        val trabajadorUpdate = trabajadorController.getTrabajadorById(trabajador[0].id)
+        trabajadorUpdate?.let {
+            it.email = "emailCambiado@otroemail.com"
+            trabajadorController.updateTrabajador(it)
+        }
+        println(trabajadorUpdate)
+        //Delete
+        val trabajadorDelete = trabajadorController.getTrabajadorById(trabajador[0].id)
+        trabajadorDelete?.let { if (trabajadorController.deleteTrabajador(it)) println("Trabajador eliminado") }
+
+        // ENCORDADORAS
+        // FindAll
+        val encordadoras = encordadoraController.getEncordadoras()
+        println(encordadoras)
+        // FindById
+        val encordadora = encordadoraController.getEncordadoraById(encordadoras[1].id)
+        println(encordadora)
+        // Update
+        encordadora?.let {
+            it.isManual = false
+            encordadoraController.updateEncordadora(it)
+        }
+        println(encordadoraController.getEncordadoras())
+        // Delete
+        encordadora?.let {
+            encordadoraController.deleteEncordadora(it)
+        }
+        println(encordadoraController.getEncordadoras())
+        // PERSONALIZADORAS
+        // FindAll
+        val personalizadoras = personalizadoraController.getPersonalizadoras()
+        println(personalizadoras)
+        // FindById
+        val personalizadora = personalizadoraController.getPersonalizadoraById(personalizadoras[1].id)
+        println(personalizadoras)
+        // Update
+        personalizadora?.let {
+            it.maniobrabilidad = false
+            personalizadoraController.updatePersonalizadora(it)
+        }
+        println(personalizadoraController.getPersonalizadoras())
+        // Delete
+        personalizadora?.let {
+            personalizadoraController.deletePersonalizadora(it)
+        }
+        println(personalizadoraController.getPersonalizadoras())
+
+        // TURNOS
+        // FindAll
+        val turnos = turnoController.getTurnos()
+        println(turnos)
+        // FindById
+        val turno = turnoController.getTurnoById(turnos[0].id)
+        println(turno)
+        // Update
+        turno?.let {
+            it.encordadora = null
+            turnoController.updateTurno(it)
+        }
+        println(turnoController.getTurnos())
+        // Delete
+        turno?.let {
+            turnoController.deleteTurno(it)
+        }
+        println(turnoController.getTurnos())
+
+        // TAREAS
+        // FindAll
+        val tareas = tareaController.getTareas()
+        println(tareas)
+        // FindById
+        val tarea = tareaController.getTareaById(tareas[0].id)
+        println(tarea)
+        // Update
+        tarea?.let {
+            it.trabajador = getTrabajadorInit()[2]
+            tareaController.updateTarea(it)
+        }
+        println(tareaController.getTareas())
+        // Delete
+        tarea?.let {
+            tareaController.deleteTarea(it)
+        }
+        println(tareaController.getTareas())
+
 
     }
-
-    //CRUD
-    //Producto
-    //FindAll
-    val producto = productoController.getProductos()
-    producto.forEach { println(it) }
-    //FindById
-    val productoId = productoController.getProductoById(producto[0].id)
-    productoId?.let { println(it) }
-    //Update
-    val productoUpdate = productoController.getProductoById(producto[0].id)
-    productoUpdate?.let {
-        it.stock += 3
-        productoController.updateProducto(it)
-    }
-    println(productoUpdate)
-    //Delete
-    val productoDelete = productoController.getProductoById(producto[0].id)
-    productoDelete?.let { if (productoController.deleteProducto(it)) println("Producto eliminado") }
-
-    //Adquisicion
-    //FindAll
-    val adquisicion = adquisicionController.getAdquisiciones()
-    adquisicion.forEach { println(it) }
-    //FindById
-    val adquisicionId = adquisicionController.getAdquisicionById(adquisicion[0].id)
-    adquisicionId?.let { println(it) }
-    //Update
-    val adquisicionUpdate = adquisicionController.getAdquisicionById(adquisicion[0].id)
-    adquisicionUpdate?.let {
-        it.cantidad += 1
-        it.precio = it.precio?.times(it.cantidad)
-        adquisicionController.updateAdquisicion(it)
-    }
-    println(adquisicionUpdate)
-    //Delete
-    val adquisicionDelete = adquisicionController.getAdquisicionById(adquisicion[0].id)
-    adquisicionDelete?.let { if (adquisicionController.deleteAdquisicion(it)) println("Adquisicion eliminada") }
-
-    //Encordar
-    //FindAll
-    val encordar = encordarController.getEncordados()
-    encordar.forEach { println(it) }
-    //FindById
-    val encordadoId = encordarController.getEncordadoById(encordar[0].id)
-    encordadoId?.let { println(it) }
-    //Update
-    val encordadoUpdate = encordarController.getEncordadoById(encordar[0].id)
-    encordadoUpdate?.let {
-        it.nudos += 2
-        encordarController.updateEncordado(it)
-    }
-    println(encordadoUpdate)
-    //Delete
-    val encordadoDelete = encordarController.getEncordadoById(encordar[0].id)
-    encordadoDelete?.let { if (encordarController.deleteEncordado(it)) println("Encordado eliminado") }
-
-    //Personalizar
-    //FindAll
-    val personalizar = personalizarController.getPersonalizaciones()
-    personalizar.forEach { println(it) }
-    //FindById
-    val personalizarId = personalizarController.getPersonalizacionById(personalizar[0].id)
-    personalizarId?.let { println(it) }
-    //Update
-    val personalizarUpdate = personalizarController.getPersonalizacionById(personalizar[0].id)
-    personalizarUpdate?.let {
-        it.peso += 1
-        personalizarController.updatePersonalizacion(personalizarUpdate)
-    }
-    println(personalizarUpdate)
-    //Delete
-    val personalizarDelete = personalizarController.getPersonalizacionById(personalizar[0].id)
-    personalizarDelete?.let { if (personalizarController.deletePersonalizacion(it)) println("Personalización eliminada") }
-
-    //Encargado
-    //FindAll
-    val encargado = encargadoController.getEncargados()
-    encargado.forEach { println(it) }
-    //FindById
-    val encargadoId = encargadoController.getEncargadoById(encargado[0].id)
-    encargadoId?.let { println(it) }
-    //Update
-    val encargadoUpdate = encargadoController.getEncargadoById(encargado[0].id)
-    encargadoUpdate?.let {
-        it.nombre = "Ernesto"
-        encargadoController.updateEncargado(it)
-    }
-    println(encargadoUpdate)
-    //Delete
-    val encargadoDelete = encargadoController.getEncargadoById(encargado[0].id)
-    encargadoDelete?.let { if (encargadoController.deleteEncargado(it)) println("Encargado eliminado") }
-
-    //Cliente
-    //FindAll
-    val cliente = clienteController.getClientes()
-    cliente.forEach { println(it) }
-    //FindById
-    val clienteId = clienteController.getClienteById(cliente[0].id)
-    clienteId?.let { println(it) }
-    //Update
-    val clienteUpdate = clienteController.getClienteById(cliente[0].id)
-    clienteUpdate?.let {
-        it.apellido = "Acosta"
-        clienteController.updateCliente(it)
-    }
-    println(clienteUpdate)
-    //Delete
-    val clienteDelete = clienteController.getClienteById(cliente[1].id)
-    clienteDelete?.let { if (clienteController.deleteCliente(it)) println("Cliente eliminado") }
-
-    //Pedido
-    //FindAll
-    val pedidos = pedidoController.getPedidos()
-    pedidos.forEach { println(it) }
-    //FindById
-    val pedidoId = pedidoController.getPedidoById(pedidos[0].id)
-    pedidoId?.let { println(it) }
-    //Update
-    val pedidoUpdate = pedidoController.getPedidoById(pedidos[0].id)
-    pedidoUpdate?.let {
-        it.estado = Pedido.TipoEstado.PROCESANDO
-        pedidoController.updatePedido(it)
-    }
-    println(pedidoUpdate)
-    //Delete
-    val pedidoDelete = pedidoController.getPedidoById(pedidos[0].id)
-    pedidoDelete?.let { if (pedidoController.deletePedido(it)) println("Pedido eliminado") }
-
-    //Trabajador
-    //FindAll
-    val trabajador = trabajadorController.getTrabajadores()
-    trabajador.forEach { println(it) }
-    //FindById
-    val trabajadorId = trabajadorController.getTrabajadorById(trabajador[0].id)
-    trabajadorId?.let { println(it) }
-    //Update
-    val trabajadorUpdate = trabajadorController.getTrabajadorById(trabajador[0].id)
-    trabajadorUpdate?.let {
-        it.email = "emailCambiado@otroemail.com"
-        trabajadorController.updateTrabajador(it)
-    }
-    println(trabajadorUpdate)
-    //Delete
-    val trabajadorDelete = trabajadorController.getTrabajadorById(trabajador[0].id)
-    trabajadorDelete?.let { if (trabajadorController.deleteTrabajador(it)) println("Trabajador eliminado") }
-
-    // ENCORDADORAS
-    // FindAll
-    val encordadoras = encordadoraController.getEncordadoras()
-    println(encordadoras)
-    // FindById
-    val encordadora = encordadoraController.getEncordadoraById(encordadoras[1].id)
-    println(encordadora)
-    // Update
-    encordadora?.let {
-        it.isManual = false
-        encordadoraController.updateEncordadora(it)
-    }
-    println(encordadoraController.getEncordadoras())
-    // Delete
-    encordadora?.let {
-        encordadoraController.deleteEncordadora(it)
-    }
-    println(encordadoraController.getEncordadoras())
-    // PERSONALIZADORAS
-    // FindAll
-    val personalizadoras = personalizadoraController.getPersonalizadoras()
-    println(personalizadoras)
-    // FindById
-    val personalizadora = personalizadoraController.getPersonalizadoraById(personalizadoras[1].id)
-    println(personalizadoras)
-    // Update
-    personalizadora?.let {
-        it.maniobrabilidad = false
-        personalizadoraController.updatePersonalizadora(it)
-    }
-    println(personalizadoraController.getPersonalizadoras())
-    // Delete
-    personalizadora?.let {
-        personalizadoraController.deletePersonalizadora(it)
-    }
-    println(personalizadoraController.getPersonalizadoras())
-
-    // TURNOS
-    // FindAll
-    val turnos = turnoController.getTurnos()
-    println(turnos)
-    // FindById
-    val turno = turnoController.getTurnoById(turnos[0].id)
-    println(turno)
-    // Update
-    turno?.let {
-        it.encordadora = null
-        turnoController.updateTurno(it)
-    }
-    println(turnoController.getTurnos())
-    // Delete
-    turno?.let {
-        turnoController.deleteTurno(it)
-    }
-    println(turnoController.getTurnos())
-
-    // TAREAS
-    // FindAll
-    val tareas = tareaController.getTareas()
-    println(tareas)
-    // FindById
-    val tarea = tareaController.getTareaById(tareas[0].id)
-    println(tarea)
-    // Update
-    tarea?.let {
-        it.trabajador = getTrabajadorInit()[2]
-        tareaController.updateTarea(it)
-    }
-    println(tareaController.getTareas())
-    // Delete
-    tarea?.let {
-        tareaController.deleteTarea(it)
-    }
-    println(tareaController.getTareas())
-
 }
 
 fun initDataBase() {
