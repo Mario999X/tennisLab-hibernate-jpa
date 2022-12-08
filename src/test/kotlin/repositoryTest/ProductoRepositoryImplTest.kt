1cf4b6953e098fca01dcf1a086d472fa05f88bc9
Sebastián Mendoza Acosta <jhoansebastian.mendoza@alumno.iesluisvives.org>
2022-12-08 21:39:58 +0100

    Test

---
+++ src/test/kotlin/repositoryTest/ProductoRepositoryImplTest.kt
@@ -0,0 +1,24 @@
+package repositoryTest
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class ProductoRepositoryImplTest {
+
+    @Test
+    fun findAll() {
+    }
+
+    @Test
+    fun findById() {
+    }
+
+    @Test
+    fun save() {
+    }
+
+    @Test
+    fun delete() {
+    }
+}
